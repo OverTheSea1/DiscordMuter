6a25c1cbc078a6824fe938db24c02bc75743b24c
OverTheSea1 <72745865+OverTheSea1@users.noreply.github.com>
2020-10-12 15:17:55 +0200

    Update JavaBOT Kopie.java

--- JavaBOT Kopie.java
+++ JavaBOT Kopie.java
@@ -32,32 +32,38 @@ import java.util.concurrent.TimeUnit;
 
 public class JavaBOT extends ListenerAdapter
 {
-   
+    /**
+     * This is the method where the program starts.
+     */
     public static void main(String[] args)
     {
     
-      
+        //We construct a builder for a BOT account. If we wanted to use a CLIENT account
+        // we would use AccountType.CLIENT
         try
         {
-            JDA jda = (JDA) new JDABuilder("TOKEN")         // The token of the account that is logging in.
-                    .addEventListeners(new JavaBOT())  
+            JDA jda = (JDA) new JDABuilder("NzY1MTk1NDQ1NzAxODM2ODUw.X4RRyg.exU2_7kxOLjr0Ckz0cbpuMHokS4")         // The token of the account that is logging in.
+                    .addEventListeners(new JavaBOT())  // An instance of a class that will handle events.
                     .setActivity(Activity.playing("AmongUs"))
                     .setStatus(OnlineStatus.ONLINE)
                    
                     .build();
-            jda.awaitReady();
+            jda.awaitReady(); // Blocking guarantees that JDA will be completely loaded.
             
             
             
         }
         catch (LoginException e)
         {
-           
+            //If anything goes wrong in terms of authentication, this is the exception that will represent it
             e.printStackTrace();
         }
         catch (InterruptedException e)
         {
-            
+            //Due to the fact that awaitReady is a blocking method, one which waits until JDA is fully loaded,
+            // the waiting can be interrupted. This is the exception that would fire in that situation.
+            //As a note: in this extremely simplified example this will never occur. In fact, this will never occur unless
+            // you use awaitReady in a thread that has the possibility of being interrupted (async thread usage and interrupts)
             e.printStackTrace();
         }
     }
@@ -70,43 +76,53 @@ public class JavaBOT extends ListenerAdapter
 	@Override
     public void onMessageReceived(MessageReceivedEvent event) 
     {
-        
-        JDA jda = event.getJDA();                       
-        long responseNumber = event.getResponseNumber();
+        //These are provided with every event in JDA
+        JDA jda = event.getJDA();                       //JDA, the core of the api.
+        long responseNumber = event.getResponseNumber();//The amount of discord events that JDA has received since the last reconnect.
 
-       
-        User author = event.getAuthor();                
-        Message message = event.getMessage();           
-        MessageChannel channel = event.getChannel();  
-                                                      
+        //Event specific information
+        User author = event.getAuthor();                //The user that sent the message
+        Message message = event.getMessage();           //The message that was received.
+        MessageChannel channel = event.getChannel();    //This is the MessageChannel that the message was sent to.
+                                                        //  This could be a TextChannel, PrivateChannel, or Group!
 
-        String msg1 = message.getContentDisplay();           
-        String msg = msg1.toLowerCase();                             
+        String msg1 = message.getContentDisplay();              //This returns a human readable version of the Message. Similar to
+        String msg = msg1.toLowerCase();                             // what you would see in the client.
 
-        boolean bot = author.isBot();                 
-                                                      
+        boolean bot = author.isBot();                    //This boolean is useful to determine if the User that
+                                                        // sent the Message is a BOT or not!
 
-        if (event.isFromType(ChannelType.TEXT))         
+        if (event.isFromType(ChannelType.TEXT))         //If this message was sent to a Guild TextChannel
         {
-           
+            //Because we now know that this message was sent in a Guild, we can do guild specific things
+            // Note, if you don't check the ChannelType before using these methods, they might return null due
+            // the message possibly not being from a Guild!
 
-            Guild guild = event.getGuild();             
-            TextChannel textChannel = event.getTextChannel();
-            Member member = event.getMember();         
+            Guild guild = event.getGuild();             //The Guild that this message was sent in. (note, in the API, Guilds are Servers)
+            TextChannel textChannel = event.getTextChannel(); //The TextChannel that this message was sent to.
+            Member member = event.getMember();          //This Member that sent the message. Contains Guild specific information about the User!
 
             String name;
             if (message.isWebhookMessage())
             {
                 name = author.getName();  
-                User user = member.getUser();	
-            }                                        
+                User user = member.getUser();	//If this is a Webhook message, then there is no Member associated
+            }                                           // with the User, thus we default to the author for name.
             else
             {
-                name = member.getEffectiveName();       
+                name = member.getEffectiveName();       //This will either use the Member's nickname if they have one,
+            }                                           // otherwise it will default to their username. (User#getName())
 
             System.out.printf("(%s)[%s]<%s>: %s\n", guild.getName(), textChannel.getName(), name, msg);
         }
-       
+        else if (event.isFromType(ChannelType.PRIVATE)) //If this message was sent to a PrivateChannel
+        {
+            //The message was sent in a PrivateChannel.
+            //In this example we don't directly use the privateChannel, however, be sure, there are uses for it!
+            PrivateChannel privateChannel = event.getPrivateChannel();
+
+            System.out.printf("[PRIV]<%s>: %s\n", author.getName(), msg);
+        }
     
        
         
@@ -127,6 +143,12 @@ public class JavaBOT extends ListenerAdapter
         		}
         		
         	channel.sendMessage("Der Sprachkanal ist nun stummgeschalten").queue();
+        	
+        	Random wuerfel = new Random();
+    		int augenZahl;
+    		if(wuerfel.nextInt(5)==1) {
+    			channel.sendMessage("Hier findest du den Bot, OpenSource auf GitHub: https://github.com/OverTheSea1/DiscordMuter").queue();
+    		}
         		}catch (Exception e) {
         			channel.sendMessage("Du bist in keinem Sprachkanal").queue();
         		}
@@ -142,13 +164,20 @@ public class JavaBOT extends ListenerAdapter
         		}
         		
         	channel.sendMessage("Der Sprachkanal ist nun wieder laut geschaltet").queue();
+        	Random wuerfel = new Random();
+    		int augenZahl;
+    		if(wuerfel.nextInt(5)==1) {
+    			channel.sendMessage("Hier findest du den Bot, OpenSource auf GitHub: https://github.com/OverTheSea1/DiscordMuter").queue();
+    		}
         		}catch (Exception e) {
         			channel.sendMessage("Du bist in keinem Sprachkanal").queue();
         		}
-        }
+     
        
        
        
-        		}}
-        	//Grüße aus DE Ba-Wü
-  
+        		}
+        	      }
+    				}
+        	
+  						// Grüße aus DE Ba-Wü
